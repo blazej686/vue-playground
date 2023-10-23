3b53c1923fe002533181e651f67b61c2f0995dfe
code . <blazej686@yahoo.com>
2023-10-23 15:52:14 -0600

    Vue Playground

--- src/components/ComputedPropertiesExercise.vue
+++ src/components/ComputedPropertiesExercise.vue
@@ -4,8 +4,8 @@
       <div class="col-12">
         <div class="border p-1">
           <div class="reverse-message">
-            <p>Original Message: {{}}</p>
-            <p>Computed Reversed Message: {{}}</p>
+            <p>Original Message: {{ state.message }}</p>
+            <p>Computed Reversed Message: {{ state.reversedMessage }}</p>
           </div>
         </div>
       </div>
@@ -21,11 +21,12 @@ export default {
   setup() {
     // NOTE typically state will be abstracted to a global AppState
     const state = reactive({
-      // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the messag gets reversed!
+      // After getting this to work, challenge yourself to add an input field and bind message to it, so as you type the message gets reversed!
       message: "Hello World!",
       reversedMessage: computed(() => {
+        const backwardsMessage = state.message.split("").reverse().join('')
         // all computed properties must return a value, what do you want this one to return?
-        return "";
+        return backwardsMessage;
       }),
     });
     return {
@@ -36,5 +37,4 @@ export default {
 </script>
 
 
-<style scoped>
-</style>
+<style scoped></style>
